f84465e1132cfa28dccb615d1c1380ae4c13dd04
abushahed670 <abu.shahed670@gmail.com>
2023-11-20 22:44:55 +0600

    variales discuss

--- variable.js
+++ variable.js
@@ -8,3 +8,10 @@ var sunglassName = "ray mond";
 // Boolean type variable
 
 var isHeSingle = true;
+
+//two types of number: integer:120, float:12.45,
+//variable naming convention: rules, best practice
+// math operation: +,-,*,/
+//short hand:++ -- += -=
+//toFixed
+// reminder: %
