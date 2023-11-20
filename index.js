203adbee8f87a1e6734653ed44e00040e7b05c51
abushahed670 <abu.shahed670@gmail.com>
2023-11-20 23:27:28 +0600

    index added

---
+++ index.js
@@ -0,0 +1,19 @@
+var numbers = [45, 46, 23, 25, 56, 98];
+
+//get elements by index
+ 
+console.log(numbers[5]);
+
+
+var elements = numbers[4];
+console.log(elements);
+
+//to set 33 in 5 number position, set elements by value
+
+numbers[5]= 33; 
+console.log(numbers);
+
+//finding index in elements
+
+var positionIndex= numbers.indexOf(23);
+console.log(positionIndex);
