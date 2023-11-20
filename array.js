3dc446560f8320550ecbe0151764a5e6bb5473c5
abushahed670 <abu.shahed670@gmail.com>
2023-11-20 22:57:45 +0600

    array declaration

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendsAges = [11, 21, 45, 17, 14];
+var picnicFee = [500, 2000, 4000, 150];
+var nayikas = ['mahi', 'opu', 'purnima', 'sabnoor'];
+var vowels = ['a', 'e','i', 'o', 'u'];
+
+console.log(vowels);
+
+console.log(nayikas.length);
