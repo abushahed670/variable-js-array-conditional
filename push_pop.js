3e690784fe6753eb3c927f0cc8158bfa0697ab2d
abushahed670 <abu.shahed670@gmail.com>
2023-11-21 00:29:55 +0600

    push pop

---
+++ push_pop.js
@@ -0,0 +1,19 @@
+var numbers =[ 78, 55, 30, 22, 36];
+console.log(numbers);
+
+//add new elements in last position
+numbers.push(44);
+console.log(numbers);
+
+var friends = ['balam', 'salam', 'jabbar'];
+friends.push('kaiyum');
+console.log(friends);
+
+//deleted elements from array- last elements
+var numbers =[ 78, 55, 30, 22, 36];
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+//position number
+numbers.pop(1);
+console.log(numbers);
