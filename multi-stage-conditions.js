a7c70264b94fb0195e1af775ea9353c7995ea009
abushahed670 <abu.shahed670@gmail.com>
2023-11-23 02:17:27 +0600

    multi and nested conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var myMoney = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuits = 20;
+
+if (danishPrice < myMoney){
+    console.log('danish khamu')
+}
+else if( butterBread < myMoney){
+    console.log(' buttter den taile')
+}
+else if( toastBiscuits < myMoney){
+    console.log(' taile toast dao')
+}
+else{
+    console.log('taile sodo chaa dao')
+}
