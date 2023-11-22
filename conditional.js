f657a8466ce41272eb0e570cdb91939472bb43d9
abushahed670 <abu.shahed670@gmail.com>
2023-11-23 01:22:21 +0600

    add conditional

---
+++ conditional.js
@@ -0,0 +1,24 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+//if iphone price is less than my budget, i will buy the  iphone...if & else conditional
+
+// if conditional.. false hoile result dibe na
+if (iphonePrice < myBudget){
+    console.log('i can buy iphone')
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if (chickenPrice <= myMoney){
+    console.log('murgir ran khabo')
+}
+// if else
+
+var chickenPrice = 500;
+var myMoney = 50;
+if (chickenPrice < myMoney){
+    console.log('murgir ran khabo')
+}
+else{ 
+    console.log('i will eat tomato')
+}
